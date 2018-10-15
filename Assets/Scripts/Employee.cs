50c1ef965af0cfa016c65312d1b9d25d67e98e5e
José Castillo <zuell83@gmail.com>
2018-10-15 07:08:16 +0200

    Added save manager

---
+++ Assets/Scripts/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Employee : MonoBehaviour {
+
+    public string theName;
+    public int friendliness;
+    public int helpfulness;
+    public string quirk;
+
+    public void Display (EmployeeEntry employee)
+    {
+        theName = employee.employeeName;
+        Text text = gameObject.GetComponentInParent(typeof(Text)) as Text;
+        text.text = employee.employeeName;
+        friendliness = employee.employeeFriendliness;
+        helpfulness = employee.employeeHelpfulness;
+        quirk = employee.quirk.ToString();
+    }
+}
---
+++ Assets/Scripts/EmployeeDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EmployeeDisplay : MonoBehaviour {
+
+    public Employee employeePrefab;
+
+	// Use this for initialization
+	void Start () {
+        Display();
+	}
+	
+	
+	public void Display () {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+		foreach (EmployeeEntry employee in SaveManager.ins.employeeDB.list)
+        {
+            Employee newEmployee = Instantiate(employeePrefab) as Employee;
+            newEmployee.transform.SetParent(transform, false);
+            newEmployee.Display(employee);
+        }
+	}
+}
